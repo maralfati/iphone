1962cd24f80e8b282d01740dcb0b8d04288df1c7
maralfati <fatimahabeb855@gmail.com>
2023-10-15 09:55:56 -0700

    iphone

---
+++ script.js
@@ -0,0 +1,8 @@
+let img=document.querySelector('.img')
+function phones(pho){
+    img.src=pho
+}
+let container=document.querySelector('.container')
+function color(co){
+    container.style.background=co
+}
